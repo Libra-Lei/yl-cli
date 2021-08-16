ea262973efa6e23bd00af0adcd840244f550a59e
Libra <myappname@163.com>
2021-08-16 14:37:10 +0800

    增加模板

--- utils/template-repository.js
+++ utils/template-repository.js
@@ -3,5 +3,6 @@ module.exports = [
   { name: 'vuexy-kit-4.0', value: 'direct:https://gitee.com/windmagics/vuexy-kit-4.0.git#master' },
   { name: 'vuexy-kit-4.1', value: 'direct:https://gitee.com/windmagics/vuexy-kit-4.1.git#master' },
   { name: 'Iframe Portal', value: 'direct:https://gitee.com/windmagics/vue-portal-iframe-template.git#master' },
-  { name: 'Iframe Sub System', value: 'direct:https://gitee.com/windmagics/vue-sub-system-iframe-template.git#master' }
+  { name: 'Iframe Sub System', value: 'direct:https://gitee.com/windmagics/vue-sub-system-iframe-template.git#master' },
+  { name: 'Micro APP', value: 'direct:https://gitee.com/windmagics/vue-no-navigate-micro-template.git' }
 ]
